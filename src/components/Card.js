13d190cd21f4bcae1449f44b10fb19f0ba151e06
davidbinstock <binstock.d@gmail.com>
2018-07-10 18:04:22 -0500

    changed image source property

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,10 @@ import React from 'react';
 import "./Card.css"
 
 function Card(props) {
+    const imageSrc = require(`../../public${props.image}`)
     return (
         <div className="card click-item">
-            <img className="card-img-top card-img" src={props.image} alt={props.name} onClick={()=>props.clickHandler(props.id)} data-saved={props.saved} data-id={props.id}/>
+            <img className="card-img-top card-img" src={imageSrc} alt={props.name} onClick={()=>props.clickHandler(props.id)} data-saved={props.saved} data-id={props.id}/>
         </div>
     )
 };
